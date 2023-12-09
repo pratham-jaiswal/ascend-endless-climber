0c06257dd951a640a1d40acc305e3461e255d527
pratham-jaiswal <prathamj0502@gmail.com>
2023-12-09 15:25:27 +0530

    Add Sprites

---
+++ Assets/Sprites/Joystick Pack/Scripts/Joysticks/FloatingJoystick.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class FloatingJoystick : Joystick
+{
+    private Vector2 initialBackgroundPosition;
+    protected override void Start()
+    {
+        base.Start();
+        initialBackgroundPosition = background.anchoredPosition;
+    }
+
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        background.anchoredPosition = ScreenPointToAnchoredPosition(eventData.position);
+        base.OnPointerDown(eventData);
+    }
+
+    public override void OnPointerUp(PointerEventData eventData)
+    {
+        background.anchoredPosition = initialBackgroundPosition;
+        base.OnPointerUp(eventData);
+    }
+}
